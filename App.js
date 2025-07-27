60bfa38add3b1d7f012c9dc4010931c70e880503
RamanandaSindhu <ramanandasindhura@gmail.com>
2025-07-27 12:19:35 -0700

    chapter-01

---
+++ App.js
@@ -0,0 +1,42 @@
+/*
+
+<div id = 'parent>
+    <div id = 'child'>
+        <h1 id = 'heading'>Hello, this is my 1st React code</h1>
+        <p>This is a paragraph inside the child div.</p> sibling
+    </div>
+    <div id = 'sibling'>
+        <h1 id = 'heading'>Hello, this is my 1st React code</h1>
+        <p>This is a paragraph inside the sibling div.</p>
+    </div>
+</div>
+
+*/
+//this is core React code, later we will use JSX to write this code
+
+
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+  [
+    React.createElement("div", { id: "child" }, [
+      React.createElement("h1", { id: "heading" }, "Hello, this is my 1st React code"),
+      React.createElement("p", {}, "This is a paragraph inside the child div.")
+    ]),
+    React.createElement("div", { id: "sibling" }, [
+      React.createElement("h1", { id: "heading" }, "Hello, this is my 1st React code"),
+      React.createElement("p", {}, "This is a paragraph inside the sibling div.")
+    ])
+  ]
+); //creating a parent div
+
+// const heading = React.createElement(
+//   "h1",
+//   { id: "heading" },
+//   "Hello, this is my 1st React code"
+// ); //object
+
+const root = ReactDOM.createRoot(document.getElementById("header"));
+root.render(parent); //rendering the element to the root
+
+console.log(parent);
